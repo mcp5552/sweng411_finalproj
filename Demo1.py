8d6609482cbbe9a48bd4c8c15da53dcd67547902
mcp5552 <mcp5552@psu.edu>
2023-11-03 22:44:58 -0400

    implemented DataHandler class, and getCSV() method

--- Demo1.py
+++ Demo1.py
@@ -1,30 +1,37 @@
 #!/usr/bin/env python3
 
-import awswrangler as wr
-import pandas as pd
-import boto3
-import warnings
-warnings.filterwarnings('ignore')
-import configparser
+class DataHandler: 
+    import awswrangler as wr
+    import pandas as pd
+    import boto3
+    import warnings
+    warnings.filterwarnings('ignore')
+    import configparser
 
-#reading the credentials securely
-credents = configparser.ConfigParser()
 
-#user read_file method
-credents.read_file(open('credentials.config'))
+    #reading the credentials securely
+    credents = configparser.ConfigParser()
 
-#Reading in the credentials into Python Variables
-aws_key = credents["AWS"]["KEY"]
-aws_secret = credents["AWS"]["SECRET"]
-region = credents["AWS"]["REGION"]
+    #user read_file method
+    credents.read_file(open('credentials.config'))
 
-#creating your session
-your_session = boto3.Session(aws_acces_key_id=aws+key,aws_secret_access_key=aws_secret,region_name=region)
+    #Reading in the credentials into Python Variables
+    aws_key = credents["AWS"]["KEY"]
+    aws_secret = credents["AWS"]["SECRET"]
+    region = credents["AWS"]["REGION"]
 
-#define raw data path
-raw_s3_bucket='sweng411'
-raw_path dir = 'csv/'
-raw_path = f"s3://{bucket}/{raw_path_dir}"
+    #creating your session
+    your_session = boto3.Session(aws_acces_key_id=aws_key,aws_secret_access_key=aws_secret,region_name=region)
 
-raw_df = wr.ls3.read_csv(path=raw_path, path_suffix=['.csv.'],dataset=True)
-raw_df.head()
+    #define raw data path
+    raw_s3_bucket='sweng411'
+    raw_path_dir = 'csv/'
+    raw_path = f"s3://{raw_s3_bucket}/{raw_path_dir}"
+
+    def getCSV():
+        raw_df = wr.ls3.read_csv(path=raw_path, path_suffix=['.csv.'],dataset=True)
+        return raw_df
+
+    raw_df = getCSV() 
+
+    raw_df.head()
