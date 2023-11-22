4fbeee6ce72133ad532ba8747a07de958725f26d
mcp5552 <118708046+mcp5552@users.noreply.github.com>
2023-11-22 10:32:09 -0600

    Send_csv() demo and get_parquet()

--- Demo2.py
+++ Demo2.py
@@ -35,6 +35,28 @@ def main():
     raw_df = dh.getCsv()
     print(raw_df.to_string())
 
+    '''
+    # try editing the df by dropping an element
+    new_df = raw_df.drop(columns=['<useless_column>'])
+    new_df = new_df.head(n=1) #just keep 1 entry
+    print("\n New df with no useless column:\n " + new_df.to_string())
+    
+    # try sending the new dataframe to s3
+    dh.sendCsv(new_df) #this appends to the existing csv as of now 
+
+    #try getting the new dataframe FROM s3 (to demo getCsv())    
+	df2 = dh.getCsv()
+    df2 = df2.tail(n=1) #just use the last line of the modified csv 
+    print('\nNew csv file:' + df2.to_string()) 
+    '''
+
+    #try to read/write to parquet s3 bucket (data warehouse)
+    dh.load(raw_df)
+    warehouse_data = dh.getParquet()
+    warehouse_data = warehouse_data.tail(n=1)
+    print(warehouse_data.to_string())
+
+
 class DataHandler:
     #getCsv() reads a csv file in from s3 and returns a dataframe
     @staticmethod
@@ -43,12 +65,20 @@ class DataHandler:
         return raw_df
 
     #sendCsv() writes a dataframe as a csv file to the s3 bucket for csv files 
+    @staticmethod
     def sendCsv(data_in):
-        wr.s3.to_csv(df = data_in, path = csv_path, boto3_session = your_session)
+        #testing dataset=True 
+        wr.s3.to_csv(df = data_in, path = csv_path, dataset = True, boto3_session = your_session)
 
     #load() writes a dataframe to the s3 bucket for parquet files 
+    @staticmethod
     def load(data_in):
-        wr.s3.to_parquet(df = df_in, path = parquet_path, boto3_session = your_session)
+        wr.s3.to_parquet(df = df_in, path = parquet_path, dataset=True, boto3_session = your_session)
+
+    #getParquet() reads a parquet file in the data warehouse and returns it as a CSV
+    @staticmethod
+    def getParquet():
+        raw_df = wr.s3.read_parquet(path = parquet_path, path_suffix=['.parquet'], dataset = True, boto3_session = your_session)
 
 if __name__== "__main__":
     main()
