089b06b51456591ec60c7be31efcdbc9b67be404
mcp5552 <118708046+mcp5552@users.noreply.github.com>
2023-11-22 12:08:39 -0600

    Working load() and getParquet()

--- Demo2.py
+++ Demo2.py
@@ -44,17 +44,19 @@ def main():
     # try sending the new dataframe to s3
     dh.sendCsv(new_df) #this appends to the existing csv as of now 
 
-    #try getting the new dataframe FROM s3 (to demo getCsv())    
-	df2 = dh.getCsv()
+    #try getting the new dataframe FROM s3 (to demo getCsv())
+    df2 = dh.getCsv()
     df2 = df2.tail(n=1) #just use the last line of the modified csv 
     print('\nNew csv file:' + df2.to_string()) 
     '''
 
     #try to read/write to parquet s3 bucket (data warehouse)
+    '''
     dh.load(raw_df)
+    '''
     warehouse_data = dh.getParquet()
     warehouse_data = warehouse_data.tail(n=1)
-    print(warehouse_data.to_string())
+    print('Warehouse data tail n=1 :' + warehouse_data.to_string())
 
 
 class DataHandler:
@@ -66,19 +68,20 @@ class DataHandler:
 
     #sendCsv() writes a dataframe as a csv file to the s3 bucket for csv files 
     @staticmethod
-    def sendCsv(data_in):
+    def sendCsv(df_in):
         #testing dataset=True 
-        wr.s3.to_csv(df = data_in, path = csv_path, dataset = True, boto3_session = your_session)
+        wr.s3.to_csv(df = df_in, path = csv_path, dataset = True, boto3_session = your_session)
 
     #load() writes a dataframe to the s3 bucket for parquet files 
     @staticmethod
-    def load(data_in):
+    def load(df_in):
         wr.s3.to_parquet(df = df_in, path = parquet_path, dataset=True, boto3_session = your_session)
 
     #getParquet() reads a parquet file in the data warehouse and returns it as a CSV
     @staticmethod
     def getParquet():
         raw_df = wr.s3.read_parquet(path = parquet_path, path_suffix=['.parquet'], dataset = True, boto3_session = your_session)
+        return raw_df
 
 if __name__== "__main__":
     main()
